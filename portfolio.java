b97d134b5c433335ad01351d6eed555fca3e4bcd
Churchill <148092636+ChurchillUng@users.noreply.github.com>
2025-03-04 23:28:52 +0000

    Commit

--- portfolio.java
+++ portfolio.java
@@ -4,6 +4,6 @@ public class Portfolio{
     System.out.println("About me:");
     System.out.println("I am Churchill and I work as a Product Design Engineer.\n");
     System.out.println("Education:\n");
-    Systemout.println("Bachelor's Degree in Mechanical Engineering | Curtin University 2018"\n);
+    Systemout.println("Bachelor's Degree in Mechanical Engineering | Curtin University 2018\n");
   }
 }
